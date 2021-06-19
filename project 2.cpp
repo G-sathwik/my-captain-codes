690552458c2ed51a94e9c06d2a6e578c62d15053
G-sathwik <85794755+G-sathwik@users.noreply.github.com>
2021-06-19 21:42:24 +0530

    Add files via upload

---
+++ project 2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    float a,b,c;
+    cout<<"Enter two numbers";
+    cin>>a>>b;
+    c=a*b;
+    cout<<"product="<<c;
+    return 0;
+}
